f8d8a965d9b92c0e86883c1ca693ff512b5b80f9
frantracer <frantracer@gmail.com>
2026-09-16 21:39:51 +0200

    Fix how gunicorn is executed

--- backend/linkurator_core/__main__.py
+++ backend/linkurator_core/__main__.py
@@ -33,12 +33,12 @@ class ApiServer:
 
     async def start(self) -> None:
         if self.with_gunicorn:
-            task = await asyncio.create_subprocess_shell(" ".join(
-                [".venv/bin/gunicorn", f"{self.app_path}()",
-                 "--workers", f"{self.workers}",
-                 "--worker-class", "uvicorn.workers.UvicornWorker",
-                 "--bind", f"{self.host}:{self.port}",
-                 "--access-logfile", "-"]))
+            task = await asyncio.create_subprocess_exec(
+                ".venv/bin/gunicorn", f"{self.app_path}()",
+                "--workers", f"{self.workers}",
+                "--worker-class", "uvicorn.workers.UvicornWorker",
+                "--bind", f"{self.host}:{self.port}",
+                "--access-logfile", "-")
 
             await task.wait()
         else:
